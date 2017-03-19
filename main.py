6c052b8100fc57d4b58cd222d60444df995ecae4
pn <pn.falcoz.dev@gmail.com>
2017-03-19 18:06:08 +0100

    More poc

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from datetime import datetime
 import unittest
 
 
-@freeze_time("1955-11-12")
 class MyTests(unittest.TestCase):
+
+    @freeze_time("1955-11-12")
     def test_the_class(self):
         assert datetime.now() == datetime(1955, 11, 12)
 
-        
+    def test_custom(self):
+        assert datetime.now() != datetime(1955, 11, 12)
+
+    def test_custom2(self):
+        with freeze_time('2012-01-01'):
+            assert datetime.now() == datetime(2012, 1, 1)
+        assert datetime.now() != datetime(2012, 1, 1)
+            
 if __name__ == '__main__':
     unittest.main()
+
